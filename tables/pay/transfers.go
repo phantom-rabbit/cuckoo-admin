955891d65fad6c0d1893ddb8d983350e4d953cab
phantom <phantom>
2021-06-08 16:28:40 +0800

    update transfers

--- tables/pay/transfers.go
+++ tables/pay/transfers.go
@@ -4,22 +4,25 @@ import (
 	"cuckoo-admin/database"
 	"cuckoo-pay/service"
 	"cuckoo-pay/service/module"
+	"database/sql"
 	"fmt"
 	"github.com/GoAdminGroup/go-admin/context"
 	"github.com/GoAdminGroup/go-admin/modules/db"
 	"github.com/GoAdminGroup/go-admin/plugins/admin/modules/table"
+	"github.com/GoAdminGroup/go-admin/template"
 	"github.com/GoAdminGroup/go-admin/template/types"
 	"github.com/GoAdminGroup/go-admin/template/types/action"
 	"github.com/GoAdminGroup/go-admin/template/types/form"
+	"github.com/GoAdminGroup/themes/sword/components/card"
 	"strconv"
 	"time"
+
+	htemplate "html/template"
 )
 
 func GetTransfersTable(ctx *context.Context) table.Table {
 	transfers := table.NewDefaultTable(table.DefaultConfigWithDriverAndConnection("postgresql", "cuckoo"))
 	info := transfers.GetInfo()
-
-	transfers.GetForm()
 	info.HideNewButton()
 	info.HideDeleteButton()
 
@@ -31,7 +34,7 @@ func GetTransfersTable(ctx *context.Context) table.Table {
 	info.AddField("hash", "tx_id", db.Text).FieldDisplay(func(model types.FieldModel) interface{} {
 		return `<a target="_blank" href="https://trx.tokenview.com/cn/tx/`+ model.Value + `" >` + model.Value + `</a>`
 	}).FieldFilterable()
-	//info.AddField("区块高度", "block_height", db.Int8)
+
 	info.AddField("时间", "timestamp", db.Int8).FieldDisplay(func(model types.FieldModel) interface{} {
 		unix, _ := strconv.ParseInt(model.Value, 10, 64)
 		return time.Unix(unix / 1000, 0).Format("2006-01-02 15:04:05")
@@ -53,8 +56,10 @@ func GetTransfersTable(ctx *context.Context) table.Table {
 	})
 	info.AddField("回调状态", "call_back", db.Bool)
 	info.AddField("回调次数", "call_back_num", db.Int)
-	info.AddField("归集状态", "collect", db.Bool)
-
+	info.AddField("归集状态", "collect", db.Bool).FieldFilterable(types.FilterType{FormType: form.SelectSingle}).FieldFilterOptions(types.FieldOptions{
+		{Value: "0", Text: "失败"},
+		{Value: "1", Text: "成功"},
+	})
 
 	info.AddActionButton("回调", action.PopUp("callback", "回调结果", func(ctx *context.Context) (success bool, msg string, data interface{}) {
 		value := ctx.FormValue("id")
@@ -106,6 +111,7 @@ func GetTransfersTable(ctx *context.Context) table.Table {
 		return true, "ok", "归集成功,交易Hash:" + collect
 	}))
 
+	info.SetHeaderHtml(TransferHeaderHtml(ctx))
 	info.SetTable("transfers").SetTitle("充值记录").SetDescription("充值记录列表").SetNoCompress()
 
 	detail := transfers.GetDetail()
@@ -130,5 +136,52 @@ func GetTransfersTable(ctx *context.Context) table.Table {
 	formList.HideContinueNewCheckBox()
 
 	formList.SetTable("transfers").SetTitle("设置回调地址").SetDescription("Transfers")
+
 	return transfers
+}
+
+type RequestStruct struct {
+	Hash  string `json:"hash"`
+	AppId string `json:"app_id"`
+	To    string `json:"to"`
+	Collect string `json:"collect"`
+}
+
+func TransferHeaderHtml(ctx *context.Context) htemplate.HTML {
+	var sqlQuery = `select sum(to_number("value", '99999.99')) from transfers where 1 = 1`
+	var params []interface{}
+	if ctx.Query("to") != "" {
+		sqlQuery += `and to = ?`
+		params = append(params, ctx.Query("to"))
+	} else if  ctx.Query("collect") != "" {
+		sqlQuery += `and collect = ?`
+		params = append(params, ctx.Query("collect"))
+	} else if  ctx.Query("app_id") != "" {
+		sqlQuery += `and app_id = ?`
+		params = append(params, ctx.Query("app_id"))
+	} else if  ctx.Query("tx_id") != "" {
+		sqlQuery += `and tx_id = ?`
+		params = append(params, ctx.Query("tx_id"))
+	}
+
+	var value sql.NullFloat64
+	err := database.Conn.Raw(sqlQuery, params).Scan(&value).Error
+	if err != nil {
+		fmt.Println("query err:", err)
+	}
+
+	var todayValue sql.NullFloat64
+	sqlQuery += "created_at > CURRENT_DATE"
+	err = database.Conn.Raw(sqlQuery, params).Scan(&todayValue).Error
+	if err != nil {
+		fmt.Println("query err:", err)
+	}
+
+	cardcard := card.New().
+		SetTitle("充值USDT").
+		SetSubTitle("$ " + fmt.Sprintf("%v", value.Float64)).
+		SetFooter(template.HTML(`今日充值 <strong style="margin-left:8px;">`+ "$ " + fmt.Sprintf("%v", todayValue.Float64) + `</strong>`))
+	infobox := cardcard.GetContent()
+
+	return infobox
 }
